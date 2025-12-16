a9d91002e08ec11511f4efecf5b2b7a772be3238
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2025-12-16 10:28:40 +0000

    Configure external links to open in new tab
    Add rst_prolog to configure external links to open in a new tab.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -306,3 +306,9 @@ if os.environ.get("READTHEDOCS", "") == "True":
     if "html_context" not in globals():
         html_context = {}
     html_context["READTHEDOCS"] = True
+
+# To make external links open in a new tab
+rst_prolog = """
+.. role:: raw-html(raw)
+   :format: html
+"""
